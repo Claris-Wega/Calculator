5ff09d181b54bb3a497906e97a4f9c944e660418
Claris <clariswega@gmail.com>
2025-07-13 23:35:10 -0700

    update index.html

---
+++ script.js
@@ -0,0 +1,37 @@
+// Step 1: Basic math functions
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (b === 0) {
+        return 'Error';
+    }
+    return a / b;
+}
+
+// Step 2: Variables for the operation
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+
+// Step 3: Operate function
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
